8f34820f0350a06d9201f13504447ca1c355c80f
muhammadhussnainsaeed <m.hussnainsaeed@gmail.com>
2024-05-31 21:58:34 +0500

    new classes

--- lib/main.dart
+++ lib/main.dart
@@ -536,7 +536,9 @@ class _SixthPageState extends State<SixthPage> {
   void initState() {
     super.initState();
     Future.delayed(Duration(seconds: 2), () {
-      Navigator.of(context).pop(); // or navigate to another page
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => MainPage()),
+      );
     });
   }
 
@@ -575,6 +577,137 @@ class _SixthPageState extends State<SixthPage> {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    VoteScreen(),
+    SettingsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: Container(
+        width: 390, // Set the width of the container
+        height: 95, // Set the height of the container
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100.0), // Adjust the border radius as needed
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: SizedBox(
+                width: 30, // Set the width of the icon
+                height: 30, // Set the height of the icon
+                child: ImageIcon(
+                  AssetImage(_selectedIndex == 0
+                      ? 'assets/icons/homegreen.png'
+                      : 'assets/icons/home.png'),
+                ),
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: SizedBox(
+                width: 30, // Set the width of the icon
+                height: 30, // Set the height of the icon
+                child: ImageIcon(
+                  AssetImage(_selectedIndex == 1
+                      ? 'assets/icons/votegreen.png'
+                      : 'assets/icons/vote.png'),
+                ),
+              ),
+              label: 'Vote',
+            ),
+            BottomNavigationBarItem(
+              icon: SizedBox(
+                width: 30, // Set the width of the icon
+                height: 30, // Set the height of the icon
+                child: ImageIcon(
+                  AssetImage(_selectedIndex == 2
+                      ? 'assets/icons/settingsgreen.png'
+                      : 'assets/icons/settings.png'),
+                ),
+              ),
+              label: 'Settings',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Color(0xFF00A153),
+          unselectedItemColor: Colors.grey,
+          selectedLabelStyle: TextStyle(fontSize: 14,fontWeight: FontWeight.bold), // Custom font size for selected label
+          unselectedLabelStyle: TextStyle(fontSize: 12), // Custom font size for unselected label
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Home Screen',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
+
+class VoteScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Vote Screen',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    backgroundColor: Colors.grey[100], // Set the background color to a light gray
+    body: Center(
+      child: Text(
+        'Settings Screen',
+        style: TextStyle(fontSize: 24),
+       ),
       ),
     );
   }
